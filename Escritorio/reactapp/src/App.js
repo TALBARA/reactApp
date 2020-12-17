368ae31a4f2bbbc8f3de17e38a05e2cfe870eb2b
w4_4500 .gif <mariobarasona@gmail.com>
2020-12-17 13:11:20 +0100

    Inicio de proyecto en react

---
+++ Escritorio/reactapp/src/App.js
@@ -0,0 +1,11 @@
+//Aqui solo se importa el nombre de el componente ej "import Greet from './greet'"
+
+function App() {
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
